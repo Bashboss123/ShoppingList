6201cfe6285a48c0825addb68a7f0e243687eb57
Sasha <sashaf1700@icloud.com>
2021-03-29 13:47:13 +0900

    slide 6

---
+++ ShoppingList/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Sasha Fujiwara on 2021/03/29.
+//
+
+import Foundation
+class item{
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
+        var items: [item] = []
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        let item1 = item(name: "Milk")
+        let item2 = item(name: "Eggs")
+    items = [item1, item2]
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cellid", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
+        
     }
 
 
-}
 
