e4172e66407bf62d590e203f3cf24cbb9a465403
Sasha <sashaf1700@icloud.com>
2021-04-02 16:35:16 +0900

    hahaha

--- ShoppingList/DetailViewController.swift
+++ ShoppingList/DetailViewController.swift
@@ -8,22 +8,45 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet weak var quantityLabel: UILabel!
+    
+    @IBOutlet weak var detailViewController: UILabel!
+    @IBOutlet weak var newQuantityTextField: UITextField!
+    var item: item!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.title = item.name
+        quantityLabel.text = "quantity: \(item.quantity)"
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeQuantityButton(_ sender: UIButton) {
+        if newQuantityTextField.text != "" {
+        let newQuantity = newQuantityTextField.text
+            quantityLabel.text = "quantity: \(newQuantity!)"
+        
+        }
     }
-    */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nvc = segue.destination as! ViewController
+        if newQuantityTextField.text != "" {
+            let item = items[indexPath.row]
+            let newQuantity = Int(newQuantityTextField.text!)
+            item.quantity = newQuantity!
+            
+            
+            
+            
+            
+       
+        
+    }
+    
+    
 
+}
 }
--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var newItemTextField: UITextField!
     var items: [item] = []
     
+    
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         
@@ -20,6 +21,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         tableView.dataSource = self
         let item1 = item(name: "Milk", quantity: 2)
         let item2 = item(name: "Eggs", quantity: 5)
+    
     items = [item1, item2]
     }
 
@@ -63,6 +65,26 @@ class ViewController: UIViewController, UITableViewDataSource {
             tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
         }
         
+    }
+    
+
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        let point = sender.location(in: tableView)
+        for row in tableView.visibleCells {
+            if row.frame.contains(point) {
+                performSegue(withIdentifier: "Edit", sender: nil)
+            }
+            
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let indexPath = tableView.indexPathForSelectedRow {
+    let item = items[indexPath.row]
+       
+    let vc = segue.destination as! DetailViewController
+    
+    vc.item = item
+    }
     }
     
     
